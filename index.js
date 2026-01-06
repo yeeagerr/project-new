dc4093faf993cec98b1b387feb76e907fbc35b68
Habib Abdillah <mhabibabdillah083@gmail.com>
2026-01-06 22:48:57 +0700

    update

--- index.js
+++ index.js
@@ -48,3 +48,53 @@ accordions.forEach((item) => {
     setActive(item);
   });
 });
+
+const srvAccordions = document.querySelectorAll(".srv-acc-item");
+const srvActiveColor = "text-[#EF815E]";
+const srvInactiveColor = "text-gray-500";
+
+function setSrvInactive(item) {
+  item.dataset.srvActive = "false";
+  item.querySelector(".srv-acc-content").style.maxHeight = "0";
+
+  const icon = item.querySelector(".srv-acc-icon");
+  const number = item.querySelector(".srv-acc-number");
+  const title = item.querySelector(".srv-acc-title");
+
+  icon.classList.add("rotate-180");
+  icon.classList.remove(srvActiveColor);
+  icon.classList.add(srvInactiveColor);
+
+  number.classList.remove(srvActiveColor);
+  number.classList.add(srvInactiveColor);
+
+  title.classList.remove(srvActiveColor);
+  title.classList.add(srvInactiveColor);
+}
+
+function setSrvActive(item) {
+  item.dataset.srvActive = "true";
+  item.querySelector(".srv-acc-content").style.maxHeight = "500px";
+
+  const icon = item.querySelector(".srv-acc-icon");
+  const number = item.querySelector(".srv-acc-number");
+  const title = item.querySelector(".srv-acc-title");
+
+  icon.classList.remove("rotate-180");
+  icon.classList.remove(srvInactiveColor);
+  icon.classList.add(srvActiveColor);
+
+  number.classList.remove(srvInactiveColor);
+  number.classList.add(srvActiveColor);
+
+  title.classList.remove(srvInactiveColor);
+  title.classList.add(srvActiveColor);
+}
+
+srvAccordions.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.dataset.srvActive === "true") return;
+    srvAccordions.forEach((acc) => setSrvInactive(acc));
+    setSrvActive(item);
+  });
+});
